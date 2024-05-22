e4772583b8040010de15a50de2d6641f037d4a98
Usuario <cristiandavid.corrales@gmail.com>
2024-05-21 21:58:16 -0500

    Organización de los directiorios del proyecto

---
+++ src/containers/Header/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const MyHeader = () => {
+  return (
+    <header>
+      <div className="header__container"></div>
+    </header>
+  );
+};
+
+export default MyHeader;
---
+++ src/pages/App/App.js
@@ -0,0 +1,12 @@
+import MyHeader from "../../containers/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MyHeader />
+    </div>
+  );
+}
+
+export default App;
